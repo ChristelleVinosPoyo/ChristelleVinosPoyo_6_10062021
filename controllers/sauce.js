efb0dab4d74193cad3f35f535ba5e8a7a1d3e8f5
Christelle <christellevinospoyo@gmail.com>
2021-06-18 10:18:51 +0200

    recherches bug route PUT

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,14 +1,31 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find() //pour retourner la liste complète des sauces
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error }));
+  };
+
+// exports.createSauce = (req, res, next) => {
+//   const thingObject = JSON.parse(req.body.thing);
+//   delete sauceObject._id;
+//   const sauce = new Thing({
+//     ...sauceObject,
+//     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//   });
+//   thing.save()
+//     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+//     .catch(error => res.status(400).json({ error }));
+// };
 exports.createSauce = (req, res, next) => {
-  const thingObject = JSON.parse(req.body.thing);
+  const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
-  const sauce = new Thing({
+  const sauce = new Sauce({
     ...sauceObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-  thing.save()
+  sauce.save()
     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
     .catch(error => res.status(400).json({ error }));
 };
@@ -16,12 +33,6 @@ exports.createSauce = (req, res, next) => {
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id }) //pour retourner une sauce à partir de son id
       .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(400).json({ error }));
-  };
-
-exports.getAllSauce = (req, res, next) => {
-    Sauce.find() //pour retourner la liste complète des sauces
-      .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({ error }));
   };
 
@@ -35,6 +46,18 @@ exports.modifySauce = (req, res, next) => {
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
 };
+
+//code d'Emilie :
+// exports.modifySauce = (req, res, next) => {//condition ternaire//
+//   const sauceObject = req.file ?//si req.file(fichier image) existe//
+//     {
+//       ...JSON.parse(req.body.sauce),
+//       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`//on modifie l'URL de l'image de manière dynamique//
+//     } : { ...req.body };//si req.file n'existe pas on fait une copie de req.body//
+//   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })//on passe en premier argument l'objet qu'on souhaite modifier et en deuxième argument la nouvelle version de l'objet
+//     .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+//     .catch(error => res.status(400).json({ error }));
+// };
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
@@ -51,3 +74,7 @@ exports.deleteSauce = (req, res, next) => {
     })
     .catch( error => { res.status(500).json({ error: error});});
 };
+
+// exports.likeSauce = (req, res, next) => {
+//   console.log(req.body);
+// };
--- routes/sauce.js
+++ routes/sauce.js
@@ -4,14 +4,12 @@ const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.post('/', auth, multer, sauceCtrl.createSauce);
-
-router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-
-router.get('/:id', auth, sauceCtrl.getOneSauce);
-
 router.get('/', auth, sauceCtrl.getAllSauce);
-
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+// router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
